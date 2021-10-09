b4f194bdc4cbe0f9fdee75fc9e45696a7852b96f
Joseph Morales Silva <josephzx1@hotmail.com>
2021-10-09 16:19:43 -0600

    PUT, PATCH y DELETE

--- routes/categories.router.js
+++ routes/categories.router.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const router = Router();
-router.get('/categories/:categoryId/products/:productId', (req, res) => {
+router.get('/:categoryId/products/:productId', (req, res) => {
   const { categoryId, productId } = req.params;
   res.json([{ productId, categoryId, name: `Product 1`, price: 3000 }]);
 });
